0aa3923a04406c866d527c6ef9b0b535bc2950c4
AceOluwapelumi <dagrandmaster01@gmail.com>
2022-10-11 20:35:07 +0100

    Reviewed by Ace

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     screens: {
-      sm:'480px',
-      md:'768px',
-      lg:'976px',
-      xl:'1440px'
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-image':
+          "linear-gradient(to right bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('../img/header_image.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
